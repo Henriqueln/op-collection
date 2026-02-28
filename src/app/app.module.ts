b72eb217bfdd62d6f12fcbfb5d4b2eb3efe3d773
Henrique Nascimento <henriquen@agap2.pt>
2026-02-28 18:04:16 +0000

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MarketWatchComponent } from './market-watch/market-watch.component';
 import { PlaysetsComponent } from './playsets/playsets.component';
 import { TournamentReportComponent } from './tournament-report/tournament-report.component';
 import { NgChartsModule } from 'ng2-charts';
 
-@NgModule({
-  declarations: [		
-    AppComponent,
-      MarketWatchComponent,
-      PlaysetsComponent,
-      TournamentReportComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    NgChartsModule
-  ],
-  providers: [ApiService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MarketWatchComponent,
+        PlaysetsComponent,
+        TournamentReportComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgChartsModule], providers: [ApiService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
